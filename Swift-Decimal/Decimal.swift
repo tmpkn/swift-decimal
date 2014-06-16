b46d943bedf52edd2727c0fe8da17271a8303352
Tomasz Pawelek <tompaw@tmp-imac.local>
2014-06-16 12:31:33 +0200

    Initial Commit, Hello Swift

---
+++ Swift-Decimal/Decimal.swift
@@ -0,0 +1,259 @@
+//
+//  Decimal.swift
+//  Swift-Decimal
+//
+//  Created by Tom Pawelek on 6/16/14.
+//  Copyright (c) 2014 Tom Pawelek. All Rights Reserved
+//
+//  @tmpkn
+//
+
+import Foundation
+
+struct Decimal: Printable, LogicValue, FloatLiteralConvertible, IntegerLiteralConvertible, StringLiteralConvertible {
+    var maxDigits: Int = 10
+    var decimalPlaces: Int = 2
+    var decimalBase: Int {
+    var retBase = 1
+        for _ in 0..self.decimalPlaces {
+            retBase *= 10
+        }
+        return retBase
+    }
+    var decimalTreshold: Int {
+    var treshold = 9
+        var multiplier = 1
+        for _ in 0..(self.decimalPlaces - 1) {
+            multiplier *= 10
+            treshold += 9 * multiplier
+        }
+        return treshold
+    }
+    var isSignMinus: Bool = false
+    var multiplier: Int {
+    return self.isSignMinus ? -1 : 1
+    }
+    var valWhole: Int = 0
+    var valPart: Int = 0
+    
+    init (maxDigits: Int, decimalPlaces: Int) {
+        self.maxDigits = maxDigits
+        self.decimalPlaces = decimalPlaces
+    }
+    
+    init (initialValue: Double, maxDigits: Int = 10, decimalPlaces: Int = 2) {
+        var retBase = 1
+        for _ in 0..decimalPlaces {
+            retBase *= 10
+        }
+        self.maxDigits = maxDigits
+        self.decimalPlaces = decimalPlaces
+        self.valWhole = Int(abs(initialValue) / 1.0)
+        self.valPart = lrint((abs(initialValue) % 1) * Double(retBase))
+        if initialValue < 0.0 {
+            self.isSignMinus = true
+        }
+    }
+    
+    init (initialValue: String, maxDigits: Int = 0, decimalPlaces: Int = 0) {
+        var dotFound = 0, i = 0
+        var strValueWhole = "", strValuePart = ""
+        var firstChar: Character = " "
+        for character in initialValue {
+            if i == 0 {
+                firstChar = character
+            }
+            if character == "." {
+                dotFound = i
+            } else {
+                if dotFound == 0 {
+                    strValueWhole += character
+                } else {
+                    strValuePart += character
+                }
+            }
+            ++i
+        }
+        if let valueWhole = strValueWhole.toInt() {
+            if dotFound != 0 {
+                if let valuePart = strValuePart.toInt() {
+                    self.valWhole = abs(valueWhole)
+                    self.valPart = abs(valuePart)
+                    self.isSignMinus = (firstChar == "-")
+                    self.maxDigits = max(maxDigits, countElements(strValueWhole) - ((firstChar == "-") ? 1 : 0))
+                    self.decimalPlaces = max(decimalPlaces, countElements(strValuePart))
+                }
+            } else {
+                self.valWhole = abs(valueWhole)
+                self.valPart = 0
+                self.isSignMinus = (firstChar == "-")
+                self.maxDigits = max(maxDigits, countElements(strValueWhole) - ((firstChar == "-") ? 1 : 0))
+                self.decimalPlaces = 1
+            }
+        }
+    }
+    
+    init (_ initialValue: String) {
+        self.init(initialValue: initialValue)
+    }
+    
+    init (rawInitialValueWhole: Int, rawInitialValuePart: Int, isSignMinus: Bool = false, maxDigits: Int = 10, decimalPlaces: Int = 2) {
+        self.maxDigits = maxDigits
+        self.decimalPlaces = decimalPlaces
+        self.valWhole = rawInitialValueWhole
+        self.valPart = rawInitialValuePart
+        self.isSignMinus = isSignMinus
+    }
+    
+    init (initialValue: Float, maxDigits: Int = 10, decimalPlaces: Int = 2) {
+        self.init(initialValue: Double(initialValue), maxDigits: maxDigits, decimalPlaces: decimalPlaces)
+    }
+    init (initialValue: Int, maxDigits: Int = 10, decimalPlaces: Int = 2) {
+        self.init(initialValue: Double(initialValue), maxDigits: maxDigits, decimalPlaces: decimalPlaces)
+    }
+    init (initialValue: Int8, maxDigits: Int = 10, decimalPlaces: Int = 2) {
+        self.init(initialValue: Double(initialValue), maxDigits: maxDigits, decimalPlaces: decimalPlaces)
+    }
+    init (initialValue: Int16, maxDigits: Int = 10, decimalPlaces: Int = 2) {
+        self.init(initialValue: Double(initialValue), maxDigits: maxDigits, decimalPlaces: decimalPlaces)
+    }
+    init (initialValue: Int32, maxDigits: Int = 10, decimalPlaces: Int = 2) {
+        self.init(initialValue: Double(initialValue), maxDigits: maxDigits, decimalPlaces: decimalPlaces)
+    }
+    init (initialValue: Int64, maxDigits: Int = 10, decimalPlaces: Int = 2) {
+        self.init(initialValue: Double(initialValue), maxDigits: maxDigits, decimalPlaces: decimalPlaces)
+    }
+    init (initialValue: UInt, maxDigits: Int = 10, decimalPlaces: Int = 2) {
+        self.init(initialValue: Double(initialValue), maxDigits: maxDigits, decimalPlaces: decimalPlaces)
+    }
+    init (initialValue: UInt8, maxDigits: Int = 10, decimalPlaces: Int = 2) {
+        self.init(initialValue: Double(initialValue), maxDigits: maxDigits, decimalPlaces: decimalPlaces)
+    }
+    init (initialValue: UInt16, maxDigits: Int = 10, decimalPlaces: Int = 2) {
+        self.init(initialValue: Double(initialValue), maxDigits: maxDigits, decimalPlaces: decimalPlaces)
+    }
+    init (initialValue: UInt32, maxDigits: Int = 10, decimalPlaces: Int = 2) {
+        self.init(initialValue: Double(initialValue), maxDigits: maxDigits, decimalPlaces: decimalPlaces)
+    }
+    init (initialValue: UInt64, maxDigits: Int = 10, decimalPlaces: Int = 2) {
+        self.init(initialValue: Double(initialValue), maxDigits: maxDigits, decimalPlaces: decimalPlaces)
+    }
+    
+    static func maxParams(_ left: Decimal, _ right: Decimal) -> (Int, Int) {
+        let maxDigits = max(left.maxDigits, right.maxDigits)
+        let decimalPlaces = max(left.decimalPlaces, right.decimalPlaces)
+        return (maxDigits, decimalPlaces)
+    }
+    
+    static func normalizePair(_ left: Decimal, _ right: Decimal) -> (Decimal, Decimal) {
+        let (maxDigits, decimalPlaces) = Decimal.maxParams(left, right)
+        let leftNormalized = left.toDecimal(maxDigits: maxDigits, decimalPlaces: decimalPlaces)
+        let rightNormalized = right.toDecimal(maxDigits: maxDigits, decimalPlaces: decimalPlaces)
+        return (leftNormalized, rightNormalized)
+    }
+    
+    func toDouble() -> Double {
+        return Double(self.multiplier) * (Double(valWhole) + (Double(valPart) / Double(decimalBase)))
+    }
+    func toString() -> String {
+        var retStr = ""
+        if self.isSignMinus {
+            retStr += "-"
+        }
+        if self.decimalPlaces > 0 {
+            var decFormat = "%0\(self.decimalPlaces)ld"
+            retStr += "\(valWhole).\(String(format:decFormat, valPart))"
+        } else {
+            retStr += "\(valWhole)"
+        }
+        return retStr
+    }
+    func toDecimal(maxDigits: Int = 10, decimalPlaces: Int = 2) -> Decimal {
+        assert(maxDigits >= self.maxDigits, "You cannot reduce the number of digits for Decimal variable")
+        var newValuePart = self.valPart
+        if decimalPlaces > self.decimalPlaces {
+            for _ in 0..(decimalPlaces - self.decimalPlaces) {
+                newValuePart *= 10
+            }
+        } else if decimalPlaces < self.decimalPlaces {
+            for _ in 0..(self.decimalPlaces - decimalPlaces) {
+                newValuePart /= 10
+            }
+        }
+        return Decimal(rawInitialValueWhole: self.valWhole, rawInitialValuePart: newValuePart, isSignMinus: self.isSignMinus, maxDigits: maxDigits, decimalPlaces: decimalPlaces)
+    }
+    
+    func addDecimal(right: Decimal) -> Decimal {
+        var tempOver = 0
+        var tempIsSignMinus = false
+        let (leftNormalized, rightNormalized) = Decimal.normalizePair(self, right)
+        var tempPart = (leftNormalized.multiplier * leftNormalized.valPart) + (rightNormalized.multiplier * rightNormalized.valPart)
+        if tempPart < -leftNormalized.decimalTreshold {
+            tempOver = -1
+            tempPart += (leftNormalized.decimalTreshold + 1)
+        } else if tempPart > leftNormalized.decimalTreshold {
+            tempOver = 1
+            tempPart -= (leftNormalized.decimalTreshold + 1)
+        }
+        var tempWhole = (leftNormalized.multiplier * leftNormalized.valWhole) + (rightNormalized.multiplier * rightNormalized.valWhole) + tempOver
+        if tempPart == 0 {
+            tempIsSignMinus = (tempWhole < 0)
+        } else if (tempPart < 0) {
+            if (tempWhole <= 0) {
+                tempIsSignMinus = true
+            } else {
+                tempWhole -= 1
+                tempPart = leftNormalized.decimalTreshold + 1 + tempPart
+                tempIsSignMinus = false
+            }
+        } else if (tempPart > 0) {
+            if (tempWhole >= 0) {
+                tempIsSignMinus = false
+            } else {
+                tempWhole += 1
+                tempPart = -leftNormalized.decimalTreshold - 1 + tempPart
+                tempIsSignMinus = true
+            }
+        }
+        return Decimal(rawInitialValueWhole: abs(tempWhole), rawInitialValuePart: abs(tempPart), isSignMinus: tempIsSignMinus, maxDigits: leftNormalized.maxDigits, decimalPlaces: leftNormalized.decimalPlaces)
+    }
+    
+    // Protocols
+    var description: String {
+    return self.toString()
+    }
+    
+    func getLogicValue() -> Bool  {
+        return (self.valPart != 0) || (self.valWhole != 0)
+    }
+    
+    static func convertFromFloatLiteral(value: FloatLiteralType) -> Decimal {
+        return Decimal(initialValue: Double(value))
+    }
+    
+    static func convertFromIntegerLiteral(value: IntegerLiteralType) -> Decimal {
+        return Decimal(initialValue: Double(value))
+    }
+    
+    static func convertFromStringLiteral(value: String) -> Decimal {
+        return Decimal(initialValue: value)
+    }
+    
+    static func convertFromExtendedGraphemeClusterLiteral(value: String) -> Decimal {
+        return Decimal(initialValue: value)
+    }
+    
+}
+
+
+@infix func + (left: Decimal, right: Decimal) -> Decimal {
+    return left.addDecimal(right)
+}
+
+@prefix func - (right: Decimal) -> Decimal {
+    return Decimal(rawInitialValueWhole: right.valWhole, rawInitialValuePart: right.valPart, isSignMinus: !right.isSignMinus, maxDigits: right.maxDigits, decimalPlaces: right.decimalPlaces)
+}
+
+@infix func - (left: Decimal, right: Decimal) -> Decimal {
+    return left.addDecimal(-right)
+}
---
+++ Swift-Decimal/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift-Decimal
+//
+//  Created by Tomasz Pawelek on 6/16/14.
+//
+//
+
+import Foundation
+
+println("Hello, World!")
+
